05f803730a548c2ba8c76fae4b2296aea7bfd70e
thenappan2010 <thenappan.2010@gmail.com>
2017-02-02 23:10:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('article-one',function(req,res)
+{
+    res.send("article one wil be served here");
+});
+
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
