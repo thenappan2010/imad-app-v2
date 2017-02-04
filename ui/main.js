dc8061769ecdb28b54f92b96f5d28fe56e2fb46d
thenappan2010 <thenappan.2010@gmail.com>
2017-02-04 06:21:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+var button= document.getElementById('count');
+var counter=0;
+button.onclick=function()
+{
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTMl=L=counter.toString();
+}
